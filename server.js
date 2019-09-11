f79326987bb157a0c1d7b24ab3357822b640860b
Kidany <kidobv@gmail.com>
2019-09-11 09:42:39 -0400

    console log the IP address and port

--- server.js
+++ server.js
@@ -41,9 +41,31 @@ app.get('*',(req,res) =>{
     }
 });
 
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+        }
+        ++alias;
+    });
+});
+
 var server = app.listen(process.env.PORT || 3000, function () {
-    var host = server.address().address
     var port = server.address().port
 
-    console.log("App listening at host=%s port=%s", host, port);
+    console.log("App listening at port=%s", port);
 });
