baf5a3a0dbba9497bc7371000bfd0b332afa542e
Kidany <kidobv@gmail.com>
2019-09-16 21:32:28 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    DB_USER:'kido_bv',
-    DB_PSW:'1250PagaMe',
-    URL_PRE:'mongodb+srv://',
-    URL_POST: '@pagame-qsiq9.mongodb.net/PagaMe_main?retryWrites=true&w=majority'
-}
-module.exports = config;
